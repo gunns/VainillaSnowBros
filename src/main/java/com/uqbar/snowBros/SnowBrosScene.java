42476e589dd5cd808119f3bb0ceff794c54737a2
Julian A Villaquiran <JULISCOUT00@HOTMAIL.COM>
2014-10-20 19:13:20 -0300

    Componentes

--- src/main/java/com/uqbar/snowBros/SnowBrosScene.java
+++ src/main/java/com/uqbar/snowBros/SnowBrosScene.java
@@ -1,18 +1,35 @@
 package com.uqbar.snowBros;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
+import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.GameScene;
+import com.uqbar.vainilla.appearances.Rectangle;
 
 public class SnowBrosScene extends GameScene{
 	private Dimension gameDimension;
 	private double velocity;
+	private GameComponent<GameScene> backGround;
+	private boolean playState = false;
+	private boolean systemPause = false;
+	
 	
 	public SnowBrosScene(Dimension dim, double velocity){
 		super();
 		
 		this.gameDimension= dim;
 		this.velocity=velocity;
+		this.buildBackground(Color.blue);
+	}
+	
+	private void buildBackground(Color color) {
+		if (backGround != null) {
+			this.removeComponent(this.backGround);
+		}
+		this.backGround = new GameComponent<GameScene>(new Rectangle(color,800,600),0, 0);
+		this.backGround.setZ(-1);
+		this.addComponent(this.backGround);
 	}
 
 }
--- src/main/java/com/uqbar/snowBros/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author JulianV
- *
- */
-package com.uqbar.snowBros;
