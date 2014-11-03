1b66d4238fa13471a7dab6a52df9385188e11c26
Julian A Villaquiran <JULISCOUT00@HOTMAIL.COM>
2014-11-03 02:17:15 -0300

    Corregido problema de visualizacion de insercion de enemigos: Arraylist debe ser de 'mobs' y no de 'GameComponents'. Mas importante aun, deben inicializarse los arrays antes de agregarle objetos, de ahi el nullPointerException.
    Visualizacion de Bros nula en el commit anterior, no se modifica a la
    espera de correcciones por parte de quien lo modifico (Gunns)

--- src/main/java/componentes/Enemigos.java
+++ src/main/java/componentes/Enemigos.java
@@ -3,25 +3,23 @@ package componentes;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
-import com.uqbar.snowBros.SnowBrosScene;
-import com.uqbar.vainilla.GameComponent;
-
-
-
 
 public class Enemigos{
 	
-	public ArrayList<GameComponent<SnowBrosScene>> mobs;
+	public ArrayList<Mob> mobs;
 	private Dimension dim; 
+	
 	public Enemigos(Dimension dim,boolean playState){
+			this.mobs = new ArrayList<Mob>();
 			this.dim=dim;
 			Mob mob1=new Mob(dim,playState);
 			mobs.add(mob1);
 			Mob mob2=new Mob(this.dim,playState);
-			mob2.setY(this.dim.getWidth()-10);
+			mob2.setY(40);
+			mob2.setX(80);
 			mobs.add(mob2);
 	}
-	public ArrayList<GameComponent<SnowBrosScene>> getEnemigos(){
+	public ArrayList<Mob> getEnemigos(){
 		return mobs;
 	}
 
