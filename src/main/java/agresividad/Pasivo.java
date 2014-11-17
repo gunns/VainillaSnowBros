283df97002906b33c5ea20940136c8db0aa2d1b9
Julian A Villaquiran <JULISCOUT00@HOTMAIL.COM>
2014-11-17 14:18:23 -0300

    merging

--- src/main/java/agresividad/Pasivo.java
+++ src/main/java/agresividad/Pasivo.java
@@ -4,24 +4,29 @@ import com.uqbar.vainilla.DeltaState;
 import componentes.Mob;
 
 public class Pasivo extends EstadoAgresividad {
-	boolean derecha = true;
+	boolean derecha;
 	
 	public Pasivo(Mob mob){
 		this.setMob(mob);
+		this.derecha=true;
 	}
 
 	// NO PUEDO HACER ANDAR EL MOVER, SI ALGUNO ME PUEDE AYUDAR LE AGRADESCO
 	public void mover(DeltaState deltaState){
 		if(this.getMob().getScene().getPlayState()){
-			while(derecha&&this.getMob().noLlegoAlFinal()&&!this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1)){
+			while(derecha&&this.getMob().noLlegoAlFinal() &&this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1))
+					{
 				this.getMob().moverALaDerecha(deltaState);
-				if(!this.getMob().noLlegoAlFinal()||this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1)){
+				if(!this.getMob().noLlegoAlFinal()||this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1))
+						{
 					derecha=false;
 				}
 			}
-			while(!derecha&&this.getMob().noLlegoAlComienzo()&&!this.getMob().getScene().terminaElPiso((this.getMob().getX()-this.getMob().getAppearance().getWidth()-1), this.getMob().getY()+this.getMob().getAppearance().getHeight()+1)){
+			while(!derecha&&this.getMob().noLlegoAlComienzo()&&!this.getMob().getScene().terminaElPiso((this.getMob().getX()-this.getMob().getAppearance().getWidth()-1), this.getMob().getY()+this.getMob().getAppearance().getHeight()+1))
+				{
 				this.getMob().moverALaIzquierda(deltaState);
-				if(!this.getMob().noLlegoAlComienzo()||this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1)){
+				if(!this.getMob().noLlegoAlComienzo()||this.getMob().getScene().terminaElPiso(this.getMob().getX()+1, this.getMob().getY()+this.getMob().getAppearance().getHeight()+1))
+						{
 					derecha=true;
 				}
 			}
